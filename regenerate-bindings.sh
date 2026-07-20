8e436bd5c545d37d160bc5454694cef9d2609e86
manuc66 <manuc66@gmail.com>
2026-07-21 00:22:01 +0200

    Pin transcribe.cpp version and add CI drift check

---
+++ regenerate-bindings.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+VERSION=$(cat TRANSCRIBE_VERSION)
+TAG="v${VERSION}"
+REPO="https://github.com/handy-computer/transcribe.cpp"
+TMPDIR=$(mktemp -d)
+
+echo "Regenerating bindings from ${TAG}..."
+
+git clone --depth 1 --branch "${TAG}" "${REPO}" "${TMPDIR}"
+dotnet run --project src/Generator -- "${TMPDIR}/bindings/rust/sys/src/transcribe_sys.rs"
+
+rm -rf "${TMPDIR}"
+echo "Done. Bindings match ${TAG}."
